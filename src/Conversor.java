f761014bc81178a680d49d272e866876b52c71b0
voxday <elias_dnl@yahoo.com>
2021-07-13 20:59:08 -0300

    agrego conversor de pesos a dolares

--- src/Conversor.java
+++ src/Conversor.java
@@ -1,3 +1,18 @@
+import java.math.BigDecimal;
+import java.util.Scanner;
+
 public class Conversor {
-    
+    public static void main(String[] args) {
+        System.out.println("Bienvenido al conversor de moneda");
+        System.out.println("Ingrese el monto en pesos" );
+        Scanner scanner = new Scanner(System.in);
+        double  pesos  = scanner.nextDouble();
+        String pesosString = Double.toString(pesos);
+        BigDecimal pesosBigDecimal = new BigDecimal(pesosString);
+        double tasaDeCambio = 0.00964;
+        String tasaDeCambioString = Double.toString(tasaDeCambio);
+        BigDecimal tasaDeCambioDecimal= new BigDecimal( tasaDeCambioString);
+        BigDecimal dolaresBigDecimal = pesosBigDecimal.multiply(tasaDeCambioDecimal);
+        System.out.println(pesos + " pesos equivalen a " + dolaresBigDecimal.toString() + " dolares");
+    }
 }
